f6e59676134d8c3b3007caa0f8f3b1e2ab3c506b
b2212070 <b2212070@photon.chitose.ac.jp>
2022-12-04 23:00:40 +0900

    lecture06ができました.

--- prmn2022a/prmn2022a/src/lecture06/Excercise6_1.java
+++ prmn2022a/prmn2022a/src/lecture06/Excercise6_1.java
@@ -1,2 +1,19 @@
-package lecture06;public class Excercise6_1 {
+package lecture06;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Excercise6_1 {
+    public static void main(String[] args) {
+        Scanner input  =new Scanner(System.in);
+        System.out.println("小数値を入力してください:");
+
+        try {
+            double n =input.nextDouble();
+            System.out.println("入力した値:"+ n);
+        }catch (InputMismatchException e){
+            System.out.println("エラー");
+        }
+
+    }
 }
